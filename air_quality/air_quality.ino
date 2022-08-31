427f2129a95d30dd3c18fda604650a989f26e5d2
Oleksii Kutuzov <oleksii.kutuzov@icloud.com>
2022-08-31 04:42:06 +0200

    Upload code and binary

--- air_quality/air_quality.ino
+++ air_quality/air_quality.ino
@@ -52,6 +52,12 @@
 #define REQUIRED   VERSION(1, 6, 0)
 #define FW_VERSION "1.3"
 
+String FirmwareVer = {
+	FW_VERSION};
+
+#define URL_fw_Version "https://raw.githubusercontent.com/oleksiikutuzov/esp32-homekit-air-quality/V4.0_test_updater/bin_version.txt"
+#define URL_fw_Bin	   "https://raw.githubusercontent.com/oleksiikutuzov/esp32-homekit-air-quality/V4.0_test_updater/esp32_air_quality.bin"
+
 #include "DEV_Sensors.hpp"
 #include "SerialCom.hpp"
 #include "Types.hpp"
@@ -62,6 +68,12 @@
 #include <ErriezMHZ19B.h>
 #include <HomeSpan.h>
 #include <SoftwareSerial.h>
+
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <HTTPUpdate.h>
+#include <WiFiClientSecure.h>
+#include "cert.h"
 
 #define BUTTON_PIN	   0
 #define LED_STATUS_PIN 26
@@ -78,9 +90,30 @@ DEV_TemperatureSensor *TEMP;
 DEV_HumiditySensor	  *HUM;
 #endif
 
+void firmwareUpdate();
+int	 FirmwareVersionCheck();
+
+unsigned long previousMillis = 0; // will store last time LED was updated
+const long	  interval		 = 60000;
+
+void repeatedCall() {
+	static int	  num			= 0;
+	unsigned long currentMillis = millis();
+	if ((currentMillis - previousMillis) >= interval) {
+		// save the last time you blinked the LED
+		previousMillis = currentMillis;
+		if (FirmwareVersionCheck()) {
+			firmwareUpdate();
+		}
+	}
+}
+
 void setup() {
 
 	Serial.begin(115200);
+
+	Serial.print("Active firmware version: ");
+	Serial.println(FirmwareVer);
 
 	String	   temp			  = FW_VERSION;
 	const char compile_date[] = __DATE__ " " __TIME__;
@@ -138,6 +171,7 @@ void setup() {
 void loop() {
 	homeSpan.poll();
 	server.handleClient();
+	repeatedCall();
 }
 
 void setupWeb() {
@@ -199,3 +233,72 @@ void setupWeb() {
 	server.begin();
 	Serial.println("HTTP server started");
 } // setupWeb
+
+void firmwareUpdate(void) {
+	WiFiClientSecure client;
+	client.setCACert(rootCACertificate);
+	t_httpUpdate_return ret = httpUpdate.update(client, URL_fw_Bin);
+
+	switch (ret) {
+	case HTTP_UPDATE_FAILED:
+		Serial.printf("HTTP_UPDATE_FAILED Error (%d): %s\n", httpUpdate.getLastError(), httpUpdate.getLastErrorString().c_str());
+		break;
+
+	case HTTP_UPDATE_NO_UPDATES:
+		Serial.println("HTTP_UPDATE_NO_UPDATES");
+		break;
+
+	case HTTP_UPDATE_OK:
+		Serial.println("HTTP_UPDATE_OK");
+		break;
+	}
+}
+
+int FirmwareVersionCheck(void) {
+	String payload;
+	int	   httpCode;
+	String fwurl = "";
+	fwurl += URL_fw_Version;
+	fwurl += "?";
+	fwurl += String(rand());
+	Serial.println(fwurl);
+	WiFiClientSecure *client = new WiFiClientSecure;
+
+	if (client) {
+		client->setCACert(rootCACertificate);
+
+		// Add a scoping block for HTTPClient https to make sure it is destroyed before WiFiClientSecure *client is
+		HTTPClient https;
+
+		if (https.begin(*client, fwurl)) { // HTTPS
+			Serial.print("[HTTPS] GET...\n");
+			// start connection and send HTTP header
+			delay(100);
+			httpCode = https.GET();
+			delay(100);
+			if (httpCode == HTTP_CODE_OK) // if version received
+			{
+				payload = https.getString(); // save received version
+			} else {
+				Serial.print("error in downloading version file:");
+				Serial.println(httpCode);
+			}
+			https.end();
+		}
+		delete client;
+	}
+
+	if (httpCode == HTTP_CODE_OK) // if version received
+	{
+		payload.trim();
+		if (payload.equals(FirmwareVer)) {
+			Serial.printf("\nDevice already on latest firmware version:%s\n", FirmwareVer);
+			return 0;
+		} else {
+			Serial.println(payload);
+			Serial.println("New firmware detected");
+			return 1;
+		}
+	}
+	return 0;
+}
